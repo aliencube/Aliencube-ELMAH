2d364eedf64c0ba5489fb06ac5696056d5c2c02a
Justin Yoo <justin.yoo@aliencube.com>
2014-05-07 11:26:32 +1000

    Added controller factory and action invoker to handle error automagically.

---
+++ SourceCodes/Aliencube.Elmah.Mvc/Factories/ElmahControllerFactory.cs
@@ -0,0 +1,30 @@
+﻿using Aliencube.Elmah.Mvc.Attributes;
+using Aliencube.Elmah.Mvc.Invokers;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Aliencube.Elmah.Mvc.Factories
+{
+    /// <summary>
+    /// This represents the controller factory entity inherited from <c>DefaultControllerFactory</c> and registered for ELMAH.
+    /// </summary>
+    public class ElmahControllerFactory : DefaultControllerFactory
+    {
+        /// <summary>
+        /// Creates the specified controller by using the specified request context.
+        /// </summary>
+        /// <param name="requestContext">The context of the HTTP request, which includes the HTTP context and route data.</param>
+        /// <param name="controllerName">The name of the controller.</param>
+        /// <returns>Returns the controller instance.</returns>
+        public override IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            var controller = base.CreateController(requestContext, controllerName);
+
+            var c = controller as Controller;
+            if (c != null)
+                c.ActionInvoker = new ElmahControllerActionInvoker(new ElmahHandleErrorAttribute());
+
+            return controller;
+        }
+    }
+}
---
+++ SourceCodes/Aliencube.Elmah.Mvc/Invokers/ElmahControllerActionInvoker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Aliencube.Elmah.Mvc.Invokers
+{
+    /// <summary>
+    /// This represents an entity responsible for invoking the action methods of a controller.
+    /// </summary>
+    public class ElmahControllerActionInvoker : ControllerActionInvoker
+    {
+        private readonly IExceptionFilter _filter;
+
+        /// <summary>
+        /// Initialises a new instance of the ElmahControllerActionInvoker class.
+        /// </summary>
+        /// <param name="filter">Exception filter instance.</param>
+        public ElmahControllerActionInvoker(IExceptionFilter filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException("filter");
+
+            this._filter = filter;
+        }
+
+        /// <summary>
+        /// Retrieves information about the action filters.
+        /// </summary>
+        /// <param name="controllerContext">The controller context.</param>
+        /// <param name="actionDescriptor">The action descriptor.</param>
+        /// <returns>Returns the value of the action-method parameter.</returns>
+        protected override FilterInfo GetFilters(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
+        {
+            var filterInfo = base.GetFilters(controllerContext, actionDescriptor);
+
+            filterInfo.ExceptionFilters.Add(this._filter);
+
+            return filterInfo;
+        }
+    }
+}
